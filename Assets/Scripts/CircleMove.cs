688970aa912c4c4ef0c4677be0800286b12a1231
Chris Kairalla <chris@mailtochris.com>
2015-03-23 16:39:08 -0400

    initial commit

---
+++ Assets/Scripts/CircleMove.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CircleMove : MonoBehaviour {
+
+	public Transform origin;
+	public float speed = 1f;
+	public float ySpeed = 10f;
+	private float dist;
+	private float initY;
+	// Use this for initialization
+	void Start () {
+		dist = Vector3.Distance(origin.position, transform.position);
+		initY = transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float x = Mathf.Sin (Time.time*speed)*dist;
+		float y = Mathf.Sin(Time.time*ySpeed);
+		float z = Mathf.Cos (Time.time*speed)*dist;
+		transform.localPosition = new Vector3(x,y,z);
+	}
+}
---
+++ Assets/Scripts/RandomMove.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomMove : MonoBehaviour {
+	public float range = 1;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//create offset that's between -range and range.
+		float x = Random.Range(-range, range);
+		float y = Random.Range(-range, range);
+		float z = Random.Range(-range, range);
+		transform.position += new Vector3(x,y,z);
+	}
+}
